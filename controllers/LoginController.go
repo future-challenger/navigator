470779eb6cdf40d21d9cfdf49d01823c60a0f0a6
Bruce Lee <home@Bruces-MacBook-Pro.local>
2015-12-15 15:00:49 +0800

    login / register

--- controllers/LoginController.go
+++ controllers/LoginController.go
@@ -10,7 +10,7 @@ import (
 )
 
 const (
-	LoginControllerTag = "LoginController"
+	loginControllerTag = "LoginController"
 )
 
 /*
@@ -23,7 +23,7 @@ type LoginController struct {
 
 func (controller *LoginController) Login(req *http.Request, r render.Render) {
 	defer func() {
-		fmt.Println(LoginControllerTag + " Error")
+		fmt.Errorf("LoginController error :- %v" + " Error")
 		if err := recover(); err != nil {
 			r.JSON(200, map[string]interface{}{"state": conf.ErrorCode["NORMAL_ERROR"], "message": "fail"})
 		}
@@ -35,7 +35,7 @@ func (controller *LoginController) Login(req *http.Request, r render.Render) {
 	password := req.FormValue("pwd")
 
 	// userModel := models.NewUserModel("{\"userCode\": \"" + userName + "\"\"password\":\"" + password + "\"}")
-	userModel = new(UserModel)
+	userModel := new(models.UserModel)
 	userModelList := userModel.ModelList(bson.M{"username": userName, "password": password})
 	if len(userModelList) > 0 {
 		r.JSON(200, map[string]interface{}{"state": conf.ErrorCode["ALL_OK"], "message": "success", "data": userModelList[0]})
--- controllers/RegisterController.go
+++ controllers/RegisterController.go
@@ -1,13 +1,49 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/martini-contrib/render"
+	"gopkg.in/mgo.v2/bson"
+	"navigator/conf"
+	"navigator/models"
 	"net/http"
+)
+
+const (
+	registerControllerTag = "RegisterController"
 )
 
 type RegisterController struct {
 }
 
 func (controller *RegisterController) Register(req *http.Request, r render.Render) {
-	r.JSON(200, map[string]interface{}{"hello": "world"})
+	defer func() {
+		fmt.Errorf("LoginController error :- %v" + " Error")
+		if err := recover(); err != nil {
+			r.JSON(200, map[string]interface{}{"state": conf.ErrorCode["NORMAL_ERROR"], "message": "fail"})
+		}
+	}()
+
+	req.ParseForm()
+
+	userInfoJson := req.FormValue("userInfo")
+	if len(userInfoJson) <= 0 {
+		r.JSON(200, map[string]interface{}{"state": conf.ErrorCode["NORMAL_ERROR"], "message": "data error"})
+		return
+	}
+
+	userModel := models.NewUserModel(userInfoJson)
+	findUserList := userModel.ModelList(bson.M{"username": userModel.UserName})
+	if len(findUserList) > 0 {
+		r.JSON(200, map[string]interface{}{"state": conf.ErrorCode["NORMAL_ERROR"], "message": "user exists"})
+		return
+	}
+
+	err := userModel.InsertModel(userModel)
+	if err != nil {
+		r.JSON(200, map[string]interface{}{"state": conf.ErrorCode["NORMAL_ERROR"], "message": "data error"})
+	} else {
+		r.JSON(200, map[string]interface{}{"state": conf.ErrorCode["ALL_OK"], "message": "success", "data": "OK"})
+	}
+
 }
